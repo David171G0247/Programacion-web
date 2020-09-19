e91f8ddee3b8862b3f45a832f81f1c3fc59a5226
171G0247 <171G0247@rcarbonifera.tecnm.mx>
2020-09-18 22:16:32 -0500

    Actividad 3

---
+++ Actividad3/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Actividad3.Models;
+
+namespace Actividad3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            villancicosContext context = new villancicosContext();
+            var villancicos = context.Villancico.OrderBy(x => x.Nombre);
+
+            return View(villancicos);
+        }
+        public IActionResult villancico(int? id)
+        {
+            if(id==null)
+            {
+                return RedirectToAction("Index");
+            }
+            villancicosContext context = new villancicosContext();
+            var villancicos = context.Villancico.FirstOrDefault(x => x.Id==id);
+
+            if(villancicos==null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(villancicos);
+            }
+
+
+        }
+    }
+}
